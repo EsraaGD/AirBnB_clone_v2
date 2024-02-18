6babbeeddd50e26941156f77716923a2e6f18851
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-02-18 23:05:58 +0300

    T7

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import Base
 from models.base_model import BaseModel
 from models.base import Base
 
