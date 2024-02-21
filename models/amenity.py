f57a0c75c48c0f50fff197680418d2afbdcc0809
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-02-21 23:01:51 +0300

    Task 10 testing

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, String
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenities class is a subclass of BaseModel and Base"""
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship("Place", secondary="place_amenities",
+                                   back_populates="amenities")
--- models/place.py
+++ models/place.py
@@ -2,10 +2,21 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
 from models.review import Review
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from models.amenity import Amenity
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 from models.base_model import Base
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'), primary_key=True,
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -36,3 +47,24 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenities = relationship("Amenity", secondary="place_amenities",
+                                 back_populates="place_amenities")
+    else:
+        @property
+        def amenities(self):
+            """ Getter attribute for amenities in FileStorage """
+            from models import storage
+            amenity_list = []
+            for amenity_id in self.amenity_id:
+                amenity = storage.get("Amenity", amenity_id)
+                if amenity:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, amenity):
+            """ Setter method to add an Amenity to Place object """
+            if isinstance(amenity, Amenity):
+                self.amenity_id.append(amenity.id)
