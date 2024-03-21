085f360445604451cca3eb99b66d3ddd335f92f7
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-22 02:13:39 +0300

    T2 and T3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display ""Hello HBNB!"" message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display "HBNB" message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Display "C" message"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', default={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_py(text):
+    """Display "Python" message"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
