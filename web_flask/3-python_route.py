e2003b001a866fb12f3cc36d959facee630f59bf
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-23 15:16:55 +0300

    Task 3 again

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def display_c(text):
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_py(text):
+def display_py(text='is cool'):
     """Display "Python" message"""
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
