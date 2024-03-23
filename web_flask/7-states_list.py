760972e3bf89aabc60aa0040b7b8685fa4c83e28
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-24 00:31:49 +0300

    Task 8 lessgo

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """Display html page with states list from A-Z"""
+    s = sorted(list(storage.all("State").values()
+    return render_template('7-state_list.html', states=s)
+
+@app.teardown_appcontext
+def teardown(error):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
