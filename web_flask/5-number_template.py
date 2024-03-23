679bd3349327c96e9036acde84a2ed87d978e4a4
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-23 14:35:47 +0300

    T5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display ""Hello HBNB!"" message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display "HBNB" message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Display "C" message"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', default={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_py(text):
+    """Display "Python" message"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Display "n is a number" message"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_num_template(n):
+    """Display "n is a number" message"""
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
