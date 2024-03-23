bebab9c15b4351d8710d79c268bee1f1c49c0965
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-24 02:08:46 +0300

    Task 8 fix 9

--- models/place.py
+++ models/place.py
@@ -22,7 +22,7 @@ association_table = Table('place_amenity', Base.metadata,
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(Integer, ForeignKey("cities.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
@@ -32,6 +32,8 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    
+    city = relationship("City")
 
     amenity_ids = []
     
