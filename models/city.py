067be6efd7d4b0e5e8b4f7614c336a91b0e760da
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-24 01:53:03 +0300

    Task 8 fix 7

--- models/city.py
+++ models/city.py
@@ -9,6 +9,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship(
         "Place", cascade='all, delete, delete-orphan', backref="cities")
