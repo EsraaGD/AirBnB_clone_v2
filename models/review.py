20dd7e8b0ce14caebd2267a07b97c85c2415a0a1
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-23 22:56:02 +0300

    Review class fix 5

--- models/review.py
+++ models/review.py
@@ -7,18 +7,15 @@ from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ Review class to store review information """
 
     __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    user = relationship("User", back_populates="reviews")
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-        user = relationship("User", back_populates="reviews")
-
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+      place_id = ""
+      user_id = ""
+      text = ""
