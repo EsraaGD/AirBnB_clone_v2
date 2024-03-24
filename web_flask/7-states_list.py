636f3bcb47770271638b408f5d84b83321cee113
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-24 03:06:15 +0300

    Task 8 fix 10

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,10 +14,10 @@ def state_list():
 
 
 @app.teardown_appcontext
-def teardown(error):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
+def teardown():
+ """Remove the current SQLAlchemy Session"""
+ storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+ app.run(host='0.0.0.0', port=5000)
